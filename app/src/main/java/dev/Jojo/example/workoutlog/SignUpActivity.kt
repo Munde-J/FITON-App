7e72f0b501d95e60dd9c1e569691fc4072a2796e
Munde-J <mundeangelah@gmail.com>
2022-06-04 10:22:58 +0300

    first commit

--- app/src/main/java/dev/Jojo/example/workoutlog/SignUpActivity.kt
+++ app/src/main/java/dev/Jojo/example/workoutlog/SignUpActivity.kt
@@ -1,14 +1,11 @@
 package dev.Jojo.example.workoutlog
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class SplashActivity : AppCompatActivity() {
+class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val intent = Intent(this,LoginActivity::class.java)
-        startActivity(intent)
-        finish()
+        setContentView(R.layout.activity_sign_up)
     }
 }
